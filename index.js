bab9f4095cb8c09236a7d24741af2dddffd5b0c6
vanishcovid <83956484+vanishcovid@users.noreply.github.com>
2022-03-02 14:38:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+const port = process.env.PORT || 3000
 var bodyParser = require("body-parser");
 var shell = require("shelljs");
 // Create application/x-www-form-urlencoded parser
@@ -93,8 +94,6 @@ app.get("/ruby/:user/:project/:file", urlencodedParser, function(req, res) {
         }
     );
 });
-var server = app.listen(process.env.PORT, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
+app.listen(port, function() {
+    console.log("running");
 });
