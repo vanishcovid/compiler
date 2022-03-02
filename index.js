6c745c8605db3e10b6a905939140a58e26445cf7
vanishcovid <83956484+vanishcovid@users.noreply.github.com>
2022-03-02 14:32:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get("/ruby/:user/:project/:file", urlencodedParser, function(req, res) {
         }
     );
 });
-var server = app.listen(8000, function() {
+var server = app.listen(process.env.PORT, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Example app listening at http://%s:%s", host, port);
